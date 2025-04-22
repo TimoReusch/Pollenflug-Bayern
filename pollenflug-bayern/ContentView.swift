464d5e5c297dda67dd25f57edbdd31db89804a6a
Timo Reusch <git@timo-reusch.de>
2025-04-22 22:23:15 +0200

    Initial Commit

---
+++ pollenflug-bayern/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pollenflug-bayern
+//
+//  Created by Timo Reusch on 22.04.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pollenflug-bayern/pollenflug_bayernApp.swift
@@ -0,0 +1,17 @@
+//
+//  pollenflug_bayernApp.swift
+//  pollenflug-bayern
+//
+//  Created by Timo Reusch on 22.04.25.
+//
+
+import SwiftUI
+
+@main
+struct pollenflug_bayernApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
